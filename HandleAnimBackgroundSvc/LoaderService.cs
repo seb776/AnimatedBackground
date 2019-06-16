8e9f9c9d371d9986b314458288894de7e45a3e6e
seb776 <sebastien.maire@epitech.eu>
2019-06-16 23:26:01 +0200

    Found a way for running UI service (as rendering in background probably blocked by this)

---
+++ HandleAnimBackgroundSvc/LoaderService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Toolkit
+{
+    public partial class LoaderService : ServiceBase
+    {
+        public LoaderService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // the name of the application to launch;
+            // to launch an application using the full command path simply escape
+            // the path with quotes, for example to launch firefox.exe:
+            //      String applicationName = "\"C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe\"";
+            String applicationName = "cmd.exe";
+
+            // launch the application
+            ApplicationLoader.PROCESS_INFORMATION procInfo;
+
+            var folder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            folder = folder.Replace("\\", "\\\\");
+            var path = "\"" + folder + "\\\\" + "DrawBehindDesktopIcons.exe\\" + "\"";
+            try
+            {
+
+            ApplicationLoader.StartProcessAndBypassUAC(path, out procInfo);
+            }
+            catch (Exception e)
+            {
+
+            File.WriteAllText("test.log", e.ToString());
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ HandleAnimBackgroundSvc/ProjectInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace Toolkit
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void loaderServiceProcessInstaller_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
